20459781b4c4870cca070789916d42070e13a9b3
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-03-18 14:54:22 -0400

    Create default project

--- src/projectCreator.js
+++ src/projectCreator.js
@@ -1,18 +1,23 @@
-const projectCreator = ()=>{
-    const myProjects = [];
+const myProjects = [];
 
-    class Project {
-        constructor (title, description, dueDate, priority){
-            this.title = title;
-            this.description = description;
-            this.dueDate = dueDate;
-            this.priority = priority;
-        };
+class Project {
+    constructor (title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    };
 
-        addProject(projectName){
-            myProjects.push(projectName);
-        };
+    addProject(projectName){
+        myProjects.push(projectName);
     };
 };
 
-export {projectCreator};
+console.table(myProjects);
+
+const defaultProject = new Project("placeholderTitle", "placeholderDescription", "placeholderDueDate", "placeholderPriority");
+defaultProject.addProject(defaultProject);
+
+console.table(myProjects);
+
+export {myProjects};
