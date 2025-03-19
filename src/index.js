1ac6e3ac57ef472b9a615ffc8176d1d16157479e
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-03-19 15:17:35 -0400

    Fix array targeting

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import "./styles.css";
 // import { myTodos, newTodo, myProjects } from "./todoCreator.js";
 import { myProjects } from "./projectCreator.js";
 
-// newTodo();
-// console.table(myTodos);
-
 console.log(myProjects);
 console.table(myProjects);
-
-console.log(myProjects.todos);
+console.log(myProjects[0].todos[1]);
+console.table(myProjects[0].todos);
