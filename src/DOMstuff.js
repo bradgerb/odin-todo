e4a0bc86541d1ffa396d13161c275599cd617a96
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-03-25 13:48:16 -0400

    Style todo containers

--- src/DOMstuff.js
+++ src/DOMstuff.js
@@ -9,33 +9,51 @@ const displayProjects = ()=>{
 
     for(let i = 0; i < myProjects.length; i++){
 
-        const card = document.createElement("div");
-        card.classList.add("card");
-        card.classList.add("larger");
-        card.textContent = `Project name: ${myProjects[i].title}`;
+        const projectContainer = document.createElement("div");
+        projectContainer.classList.add("projectContainer");
 
-        const addTodoButton = document.createElement("button");
-        addTodoButton.setAttribute("ID", `${index}`);
-        addTodoButton.classList.add("cardButton");
-        addTodoButton.textContent = "Add todo";
-        addTodoButton.addEventListener("click", newTodo);
-        card.appendChild(addTodoButton);
+            const cardContainer = document.createElement("div");
+            cardContainer.classList.add("cardContainer");
 
-        const removeProjectButton = document.createElement("button");
-        removeProjectButton.setAttribute("ID", `${index}`);
-        removeProjectButton.classList.add("cardButton");
-        removeProjectButton.textContent = "Remove Project";
-        removeProjectButton.addEventListener("click", removeProject);
-        card.appendChild(removeProjectButton);
+                const card = document.createElement("div");
+                card.classList.add("card");
+                card.classList.add("larger");
+                card.textContent = `Project name: ${myProjects[i].title}`;
 
-        const todos = document.createElement("div");
-        todos.classList.add("todoContainer");
-        // todos.textContent = `${defaultTodo}`;
-        card.appendChild(todos);
+                    const buttonContainer = document.createElement("div");
+                    buttonContainer.classList.add("buttonContainer");
+                
+                        const addTodoButton = document.createElement("button");
+                        addTodoButton.setAttribute("ID", `${index}`);
+                        addTodoButton.classList.add("cardButton");
+                        addTodoButton.textContent = "Add todo";
+                        addTodoButton.addEventListener("click", newTodo);
+                        buttonContainer.appendChild(addTodoButton);
+
+                        const removeProjectButton = document.createElement("button");
+                        removeProjectButton.setAttribute("ID", `${index}`);
+                        removeProjectButton.classList.add("cardButton");
+                        removeProjectButton.textContent = "Remove Project";
+                        removeProjectButton.addEventListener("click", removeProject);
+                        buttonContainer.appendChild(removeProjectButton);
+
+                    card.appendChild(buttonContainer);
+
+                cardContainer.appendChild(card);
+
+            projectContainer.appendChild(cardContainer);
+
+            const todos = document.createElement("div");
+            todos.classList.add("todoContainer");
+            todos.setAttribute("ID", "lowPrio");
+            todos.textContent = `${myProjects[index].todos.title}`;
+            projectContainer.appendChild(todos);
+
+        container.appendChild(projectContainer);
 
         index++;
 
-        container.appendChild(card);
+        
     };
     console.table(myProjects[0]);
     console.table(myProjects[1]);
